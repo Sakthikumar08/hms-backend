fe2b8c4f5b2e7a8feb17aa92f8e727416d903281
Sakthikumar08 <sakthikumarcricket@gmail.com>
2025-02-28 13:49:41 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -18,3 +18,17 @@ app.use("/api/patient", require("./routes/patientRoutes"));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/* const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const patientRoutes = require("./routes/patientRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/patient", patientRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); */
